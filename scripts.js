f4a16b99950b00abbc955280dfd6a5fb47ba1f80
pedromaniabot-spec <pedromaniabot@gmail.com>
2025-11-11 20:26:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,8 @@
-// Mensaje de bienvenida inicial
 alert("¡Bienvenido a la prueba JavaScript!");
 
-// Pedimos nombre y apellido
 let nombreCompleto = prompt("Por favor, ingrese su nombre y apellido:");
 
-// Mostramos una alerta personalizada con el nombre
 alert(`¡Bienvenido/a, ${nombreCompleto}!`);
 
-// Mostramos el nombre en el título principal de la página
 document.querySelector("#titulo").textContent = `¡Bienvenido/a, ${nombreCompleto}!`;
-
-// Mostramos el nombre ingresado también en otra parte de la página
 document.querySelector("#mostrarNombre").textContent = `Usuario: ${nombreCompleto}`;
